b6b181f3e44272889a7641d20aa3317ab8ccb748
Corey <regan.corey@gmail.com>
2021-10-28 23:01:04 -0700

    updated meta data

--- Sync AD With Local Computer Descriptions.ps1
+++ Sync AD With Local Computer Descriptions.ps1
@@ -8,13 +8,14 @@ Param()
 
     .DESCRIPTION
     Enter credentials used to search AD, and same or different credentials to connect to each resulting computer.
-    Searches AD for computer objects in matching the input search term, connects & pulls each of the resulting computers'  system descriptions for comparison in a displayed table.
+    Searches AD for computer objects in matching the input search term, connects & pulls each of the resulting computers' system 
+    descriptions for comparison in a displayed table.
     
     Options
     [1] Update AD with ALL non-empty local system descriptions matching the entered AD computer name search term
-        This will update in one batch all AD search results with their corresponding local system descriptions using the AD credentials supplied earlier. 
+        This will update in one batch all AD search results with their corresponding local system descriptions using the earlier supplied AD credentials.
 
-    [2] Manually approve each AD update one at a time 
+    [2] Manually approve each AD update one at a time
         This will prompt you for approval to update AD with the local system description of each AD computer search result.
 #>
 
